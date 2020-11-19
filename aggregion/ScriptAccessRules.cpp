5aa71e5be2ed1cadfeeb0f4b3e6bd50f662e760a
Vasiliy K <vasilii.kuznetsov@aggregion.com>
2020-11-19 10:25:16 +0300

    Minor fixes.

--- aggregion/ScriptAccessRules.cpp
+++ aggregion/ScriptAccessRules.cpp
@@ -6,7 +6,7 @@ namespace aggregion::sar {
 
    void upsert_trust(name self, name truster, name trustee, bool trust) {
       require_auth(Names::AggregionDmp); // truster
-      // TODO: check that truster and trustee is providers
+      // TODO: Check that truster and trustee is providers
       Tables::trusted_providers_table_t trusted{self, truster.value};
       auto it = trusted.find(trustee.value);
       if (it == trusted.end()) {
@@ -23,8 +23,7 @@ namespace aggregion::sar {
 
    void upsert_execution_approve(name self, name provider, checksum256 script_hash, bool approve) {
       require_auth(Names::AggregionDmp); // provider
-      // TODO:
-      // 1. check that provider is real provider
+      // TODO: Сheck that given provider is real provider
       auto script_id = scripts::get_script_id(self, script_hash);
       check(script_id.has_value(), "404. Script not found by given hash");
 
@@ -43,12 +42,12 @@ namespace aggregion::sar {
       auto sit = scripts.require_find(script_id.value(), "500. Script not found");
       scripts.modify(sit, self, [&](scripts::Def::Scripts& row) { row.approves_count += (approve ? 1 : -1); });
 
-      print("Success. Provider:'", provider, "' script hash:'", script_hash, "' approved:'", approve, "'");
+      print("Success. Provider:'", provider, "' Script hash:'", script_hash, "' Approved:'", approve, "'");
    }
 
 
    void upsert_script_access(name self, name owner, checksum256 script_hash, name grantee, bool granted) {
-      require_auth(Names::AggregionDmp); // provider
+      require_auth(Names::AggregionDmp); // owner
       auto script_id = scripts::get_script_id(self, script_hash);
       check(script_id.has_value(), "404. Script not found by given hash");
 
@@ -66,37 +65,31 @@ namespace aggregion::sar {
       } else {
          access.modify(it, self, [&](Def::ScriptsAccess& row) { row.granted = granted; });
       }
-      print("Success. Owner:'", owner, "' script hash:'", script_hash, "' grant access:'", granted, "'");
+      print("Success. Owner:'", owner, "' Script hash:'", script_hash, "' Grant access:'", granted, "'");
    }
 
 
    void ScriptAccessRules::trust(name truster, name trustee) {
-      require_auth(Names::AggregionDmp); // truster
       upsert_trust(get_self(), truster, trustee, true);
    }
 
    void ScriptAccessRules::untrust(name truster, name trustee) {
-      require_auth(Names::AggregionDmp); // truster
       upsert_trust(get_self(), truster, trustee, false);
    }
 
    void ScriptAccessRules::execapprove(name provider, checksum256 hash) {
-      require_auth(Names::AggregionDmp); // provider
       upsert_execution_approve(get_self(), provider, hash, true);
    }
 
    void ScriptAccessRules::execdeny(name provider, checksum256 hash) {
-      require_auth(Names::AggregionDmp); // provider
       upsert_execution_approve(get_self(), provider, hash, false);
    }
 
    void ScriptAccessRules::grantaccess(name owner, checksum256 hash, name grantee) {
-      require_auth(Names::AggregionDmp); // owner
       upsert_script_access(get_self(), owner, hash, grantee, true);
    }
 
    void ScriptAccessRules::denyaccess(name owner, checksum256 hash, name grantee) {
-      require_auth(Names::AggregionDmp); // owner
       upsert_script_access(get_self(), owner, hash, grantee, false);
    }
 }
